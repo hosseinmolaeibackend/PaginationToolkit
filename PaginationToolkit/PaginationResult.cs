1fd77ca264cf735b480fbbaeffb8af26447bfda3
HosseinMolaei <hossein.molaei.backend@gmail.com>
2025-09-01 01:27:48 +0330

    Add project files.

---
+++ PaginationToolkit/PaginationResult.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace PaginationToolkit
+{
+    public class PaginationResult<T>
+    {
+        public List<T> Data { get; set; }
+        public PaginationMetadata Metadata { get; set; }
+
+        public PaginationResult(List<T> data, int count, int pageNumber, int pageSize)
+        {
+            Data = data;
+            Metadata = new PaginationMetadata
+            {
+                TotalCount = count,
+                PageSize = pageSize,
+                CurrentPage = pageNumber,
+                TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+            };
+        }
+    }
+
+    public class PaginationMetadata
+    {
+        public int TotalCount { get; set; }
+        public int PageSize { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+
+        [JsonIgnore]
+        public bool HasPrevious => CurrentPage > 1;
+
+        [JsonIgnore]
+        public bool HasNext => CurrentPage < TotalPages;
+    }
+}
---
+++ PaginationToolkit/QueryableExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaginationToolkit
+{
+    public static class QueryableExtensions
+    {
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
+            this IQueryable<T> source,
+            int pageNumber,
+            int pageSize)
+        {
+            return await PaginatedList<T>.CreateAsync(source, pageNumber, pageSize);
+        }
+
+        public static IQueryable<T> Paginate<T>(
+            this IQueryable<T> source,
+            int pageNumber,
+            int pageSize)
+        {
+            return source
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
